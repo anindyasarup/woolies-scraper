0bd07ca7d2ba6d7f9e9fb9dac0c2ba61eb63d10b
anindyasarup <51510265+anindyasarup@users.noreply.github.com>
2025-03-01 21:26:38 +1100

    chore: configure eslint for linting

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+    lessOpinionated: true,
+    stylistic: {
+        indent: 4,
+        quotes: 'single',
+        semi: true
+    },
+    rules: {
+        'test/no-import-node-test': ['off'],
+        'no-unused-vars': ['error', {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            ignoreRestSiblings: true
+        }],
+        '@stylistic/comma-dangle': ['error', 'never'],
+        '@stylistic/arrow-parens': ['error', 'as-needed']
+    }
+});
